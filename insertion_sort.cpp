6a7e873032961de58726ba19f2ac7cd6ce257bae
Mohammad ali mazdarani <mazdarani26.gmail.com>
2023-05-09 10:48:31 -0700

    variable for get number

---
+++ insertion_sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system(
+) or input loop */
+//create namespace
+/*Branch name
+*
+*
+*/
+//prototype
+//--------------------------
+//micro
+//---------------------
+//global variable
+//-------------------
+//struct
+//-------------------
+//union
+//-------------------
+//class
+//-------------------
+int main(int argc, char** argv)
+{
+
+int n;   //variable for get number
+
+
+
+
+
+getch();
+}
+//function variable
+//--------------------------
+//functions
